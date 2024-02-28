7b4caad1627bc8500b6aa2f72d20781115ebbe96
iccugs <emahaffey1@gmail.com>
2024-02-27 22:46:36 -0600

    Fixed the footer issue not displaying properly on smaller mobile screens and added social media links to the footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
   const navigate = useNavigate();
 
   const contactOnClick = () => {
-    navigate('/contact');
-  }
+    navigate("/contact");
+  };
 
   const openingHours = {
     monday: "9:00 AM - 6:00 PM",
@@ -31,7 +31,9 @@ const Footer = () => {
         </p>
         <div className="help-section">
           <h2>Need help?</h2>
-          <button className="contact-btn" onClick={() => contactOnClick()}>Contact us</button>
+          <button className="contact-btn" onClick={() => contactOnClick()}>
+            Contact us
+          </button>
         </div>
       </div>
       <div className="hours">
@@ -44,6 +46,33 @@ const Footer = () => {
             </li>
           ))}
         </ul>
+        <div className="social-media">
+          <h2>Follow Us</h2>
+          <div className="icons">
+            <a
+              href="https://www.facebook.com/profile.php?id=61556703307041"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={process.env.PUBLIC_URL + "/facebook.png"} alt="FB"/>
+            </a>
+            <a
+              href="https://twitter.com/group3bookstore"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={process.env.PUBLIC_URL + "/twitter.png"} alt="TW"/>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/the-bookstore-94929b2b7/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={process.env.PUBLIC_URL + "/linkedin.png"} alt="IN"/>
+            </a>
+            {/* Add more social media links as needed */}
+          </div>
+        </div>
       </div>
     </div>
   );
