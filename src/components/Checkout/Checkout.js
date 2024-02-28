d861a58892ea19c2b2402d01904870fd3ce0c5e0
Lendacky, Mark <Mark_Lendacky@cable.comcast.com>
2024-02-28 11:04:22 -0500

    removed improper p tag

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,67 +1,74 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const Checkout = ({cartItems, setCartItems}) => {
-    const[paymentInfo, setPaymentInfo] = useState({
-        cardNumber:'',
-        expiryDate:'',
-        cvv:'',
-    });
-    const handlePaymentInfoChange = (e) => {
-        const{name, value} = e.target;
-        setPaymentInfo((prevPaymentInfo) => ({
-            ...prevPaymentInfo,
-            [name]: value,
-        }));
-    };
-    const handleCheckout = () => {
-        if (cartItems.length > 0 && paymentInfo.cardNumber && paymentInfo.expiryDate && paymentInfo.cvv){
-            console.log('Payment Information:', paymentInfo);
-            setCartItems();
-        } else {
-            console.log('Invalid checkout request. Please Check your cart and payment information.');
-        }
-    };
-    return (
-        <div className="checkout">
-            <h1>Checkout</h1>
-            {cartItems?.length > 0 ?(
-                <div>
-                    {cartItems?.map((item, index) => (
-                        <div key={`${index}--cartItem`}>
-                            <h2>{item.title}</h2>
-                            <p2>{item.description}</p2>
-                            <p>Price: ${item.price}</p>
-                        </div>
-                    ))}
+const Checkout = ({ cartItems, setCartItems }) => {
+  const [paymentInfo, setPaymentInfo] = useState({
+    cardNumber: "",
+    expiryDate: "",
+    cvv: "",
+  });
+  const handlePaymentInfoChange = (e) => {
+    const { name, value } = e.target;
+    setPaymentInfo((prevPaymentInfo) => ({
+      ...prevPaymentInfo,
+      [name]: value,
+    }));
+  };
+  const handleCheckout = () => {
+    if (
+      cartItems.length > 0 &&
+      paymentInfo.cardNumber &&
+      paymentInfo.expiryDate &&
+      paymentInfo.cvv
+    ) {
+      console.log("Payment Information:", paymentInfo);
+      setCartItems();
+    } else {
+      console.log(
+        "Invalid checkout request. Please Check your cart and payment information."
+      );
+    }
+  };
+  return (
+    <div className="checkout">
+      <h1>Checkout</h1>
+      {cartItems?.length > 0 ? (
+        <div>
+          {cartItems?.map((item, index) => (
+            <div key={`${index}--cartItem`}>
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+              <p>Price: ${item.price}</p>
+            </div>
+          ))}
         </div>
-    ) : (
+      ) : (
         <p>Your cart is empty.</p>
-    )}
-    <div className="payment-section">
+      )}
+      <div className="payment-section">
         <label>Card Number:</label>
         <input
-            type="text"
-            name="cardNumber"
-            value={paymentInfo.cardNumber}
-            onChange={handlePaymentInfoChange}
+          type="text"
+          name="cardNumber"
+          value={paymentInfo.cardNumber}
+          onChange={handlePaymentInfoChange}
         />
         <label>Expiry Date:</label>
         <input
-            type="text"
-            name="expiryDate"
-            value={paymentInfo.expiryDate}
-            onChange={handlePaymentInfoChange}
+          type="text"
+          name="expiryDate"
+          value={paymentInfo.expiryDate}
+          onChange={handlePaymentInfoChange}
         />
         <label>CVV</label>
         <input
-            type="text"
-            name="cvv"
-            value={paymentInfo.cvv}
-            onChange={handlePaymentInfoChange}
+          type="text"
+          name="cvv"
+          value={paymentInfo.cvv}
+          onChange={handlePaymentInfoChange}
         />
         <button onClick={handleCheckout}>Proceed to Checkout</button>
+      </div>
     </div>
-    </div>
-    );
+  );
 };
 export default Checkout;
