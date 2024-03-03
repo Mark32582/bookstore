4b10de1daf88024f860ce5a1f41e9cbb76c9cbcd
Lendacky, Mark <Mark_Lendacky@cable.comcast.com>
2024-03-03 17:50:41 -0500

    updated styling on verification

--- src/components/Verification/Verification.js
+++ src/components/Verification/Verification.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 
-const Verification = ({ purchasedItems, paymentInfo, userProfile, totalCost }) => {
+const Verification = ({
+  purchasedItems,
+  paymentInfo,
+  userProfile,
+  totalCost,
+}) => {
   const [isUserSignedIn] = useState(!!userProfile);
 
   const handleConfirmOrder = () => {
@@ -29,58 +34,61 @@ const Verification = ({ purchasedItems, paymentInfo, userProfile, totalCost }) =
 
   return (
     <div className="verification">
-      <h1>Order Confirmation</h1>
-      <h2>Items Purchased:</h2>
-      {purchasedItems?.length > 0 ? (
-        <ul>
-          {purchasedItems.map((item, index) => (
-            <li key={`${index}--verificationItem`}>{item.title}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>Your cart is empty.</p>
-      )}
+      <div className="verification--inner-container">
+        <h1>Order Confirmation</h1>
+        <h2>Items Purchased:</h2>
+        {purchasedItems?.length > 0 ? (
+          <ul>
+            {purchasedItems.map((item, index) => (
+              <li key={`${index}--verificationItem`}>{item.title}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>Your cart is empty.</p>
+        )}
 
-      <h2>Payment Information:</h2>
-      {paymentInfo && paymentInfo.cardNumber ? (
-        <>
-          <p>Order Number: BST-{Math.floor(Math.random() * 10 * 10000)}</p>
-          <p>Total cost of: {calculateTotalCost(purchasedItems)} </p>
+        <h2>Payment Information:</h2>
+        {paymentInfo && paymentInfo.cardNumber ? (
+          <>
+            <p>Order Number: BST-{Math.floor(Math.random() * 10 * 10000)}</p>
+            <p>Total cost of: {calculateTotalCost(purchasedItems)} </p>
+            <p>
+              Card Number: **** **** **** ****{" "}
+              {paymentInfo.cardNumber.slice(0, -12)}
+            </p>
+          </>
+        ) : (
           <p>
-            Card Number: **** **** **** ****{" "}
-            {paymentInfo.cardNumber.slice(0, -12)}
+            {paymentInfo
+              ? "No payment information available."
+              : "Loading payment information..."}
           </p>
-        </>
-      ) : (
-        <p>
-          {paymentInfo
-            ? "No payment information available."
-            : "Loading payment information..."}
-        </p>
-      )}
+        )}
 
-      <h2>Shipping Information:</h2>
-      {isUserSignedIn && userProfile && userProfile.name ? (
-        <>
-          <p>Name: {userProfile.name}</p>
+        <h2>Shipping Information:</h2>
+        {isUserSignedIn && userProfile && userProfile.name ? (
+          <>
+            <p>Name: {userProfile.name}</p>
+            <p>
+              Shipping Address: {userProfile?.streetNumber}{" "}
+              {userProfile?.address}{" "}
+            </p>
+            <p>
+              {userProfile?.city} {userProfile?.state} {userProfile?.zip}
+            </p>
+          </>
+        ) : (
           <p>
-            Shipping Address: {userProfile?.streetNumber} {userProfile?.address}{" "}
+            {isUserSignedIn
+              ? "No user information available."
+              : "Loading user information..."}
           </p>
-          <p>
-            {userProfile?.city} {userProfile?.state} {userProfile?.zip}
-          </p>
-        </>
-      ) : (
-        <p>
-          {isUserSignedIn
-            ? "No user information available."
-            : "Loading user information..."}
-        </p>
-      )}
-      <div>
-        <span>Thank you for shopping with us!</span>
+        )}
+        <div>
+          <span>Thank you for shopping with us!</span>
+        </div>
+        <button onClick={handleConfirmOrder}>Continue Shopping</button>
       </div>
-      <button onClick={handleConfirmOrder}>Continue Shopping</button>
     </div>
   );
 };
