a490665f73deea7c62015e6bab1344adb0eb2d4f
Lendacky, Mark <Mark_Lendacky@cable.comcast.com>
2024-03-05 20:10:17 -0500

    add books works properly now and inventory is showing alt image

--- src/components/AddBooks/AddBook.js
+++ src/components/AddBooks/AddBook.js
@@ -1,24 +1,40 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../config/fireBaseConfig";
 
 const AddBooks = (props) => {
   const { search, setSearch, books, setBooks } = props;
 
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
-  const [quantity, setQuantity] = useState("");
+
   const [selectedBook, setSelectedBook] = useState(null);
   const selectedCollections = [
-    "Best Seller",
-    "New Releases",
-    "Carousel",
-    "Employee Recommendations",
+    "bestSeller",
+    "newReleases",
+    "carousel",
+    "employeeRecommendations",
   ];
   const [orderQuantity, setOrderQuantity] = useState(1);
   const [orderSuccess, setOrderSuccess] = useState(false);
   const [orderError, setOrderError] = useState(false);
 
-  const fetchCollection = async (collectionName, setSearchResults) => {};
+  const fetchCollection = async (collectionName, setBooks) => {
+    const collectionRef = collection(db, collectionName);
+    const querySnapshot = await getDocs(collectionRef);
+    const results = querySnapshot.docs.map((doc) => ({
+      collection: collectionName,
+      id: doc.id,
+      title: doc.data().title,
+      author: doc.data().author,
+      description: doc.data().description,
+      thumbnail: doc.data().thumbnail,
+      cost: doc.data().cost,
+    }));
+    console.log(results);
+    setBooks(results);
+  };
 
   const handleSearch = () => {
     axios
@@ -29,43 +45,42 @@ const AddBooks = (props) => {
       .catch((err) => console.log(err));
   };
 
-  const handleBookClick = (book) => {
-    setSelectedBook(book);
-    setOrderQuantity(1);
+  const handleBookClick = (book, orderQuantity) => {
+    console.log("---------" + book, orderQuantity);
+    // setSelectedBook(book);
+    // setOrderQuantity(orderQuantity);
+    // console.log(selectedBook)
   };
 
-  const handleOrderQuantityChange = (event) => {
-    setOrderQuantity(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = (selectedBook) => {
     if (selectedBook) {
       const newBook = {
-        title: selectedBook.volumeInfo.title,
-        author: selectedBook.volumeInfo.authors[0],
+        title: selectedBook?.volumeInfo?.title || selectedBook?.title,
+        author: selectedBook?.volumeInfo?.authors[0] || selectedBook?.author,
         quantity: orderQuantity,
-        category: selectedBook.volumeInfo.category,
-        cost: selectedBook.volumeInfo.cost,
-        description: selectedBook.volumeInfo.description,
-        googleId: selectedBook.volumeInfo.googleId,
-        id: selectedBook.volumeInfo.id,
-        inventoryCount: selectedBook.volumeInfo.inventoryCount,
-        pageCount: selectedBook.volumeInfo.pageCount,
-        publisher: selectedBook.volumeInfo.publisher,
-        publisherDate: selectedBook.volumeInfo.publisherDate,
-        retailPrice: selectedBook.volumeInfo.retailPrice,
-        thumbnail: selectedBook.volumeInfo.thumbnail,
+        category: selectedBook?.volumeInfo?.category || selectedBook?.category,
+        cost: selectedBook?.volumeInfo?.cost || selectedBook?.cost || 8.99,
+        description:
+          selectedBook?.volumeInfo?.description || selectedBook?.description,
+        googleId: selectedBook?.volumeInfo?.googleId || selectedBook?.googleId,
+        id: selectedBook?.volumeInfo?.id || selectedBook?.id,
+        inventoryCount:
+          selectedBook?.volumeInfo?.inventoryCount || orderQuantity,
+        pageCount:
+          selectedBook?.volumeInfo?.pageCount || selectedBook?.pageCount,
+        publisher:
+          selectedBook?.volumeInfo?.publisher || selectedBook?.publisher,
+        publisherDate:
+          selectedBook?.volumeInfo?.publisherDate ||
+          selectedBook?.publisherDate,
+        retailPrice:
+          selectedBook?.volumeInfo?.retailPrice ||
+          selectedBook?.retailPrice ||
+          14.99,
+        thumbnail:
+          selectedBook?.volumeInfo?.thumbnail || selectedBook?.thumbnail,
       };
 
-      // Logic for adding the new book to the collection
-      // Example: setBooks([...books, newBook]);
-
-      setTitle("");
-      setAuthor("");
-      setQuantity("");
-      setSelectedBook(null);
-      setOrderQuantity(1);
       setOrderSuccess(true);
       setOrderError(false);
     } else {
@@ -74,107 +89,121 @@ const AddBooks = (props) => {
     }
   };
 
-  const handleBookLinkClick = (bookUrl) => {
-    console.log(`Opening book URL: ${bookUrl}`);
-    // Open the book URL in a new tab or window
-  };
+  console.log(selectedBook);
   return (
-    <div>
-      <div className="add">
-        <div className="add-container">
-          <h2>Search Books</h2>
-          <form onSubmit={handleSubmit}>
-            <ul>
-              {selectedCollections.map((collectionName) => (
-                <li key={collectionName}>
-                  <label>
-                    <input
-                      type="radio"
-                      name="collection"
-                      value={collectionName}
-                      onChange={() => fetchCollection(collectionName)}
-                    />
-                    {collectionName}
-                  </label>
-                </li>
-              ))}
-            </ul>
-          </form>
-
+    <div className="add-container">
+      {orderSuccess && (
+        <h3
+          style={{
+            color: "green",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          Your order has been placed successfully!
+        </h3>
+      )}
+      {orderError && (
+        <h3
+          style={{
+            color: "red",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          Something went wrong. Please try again in a moment.
+        </h3>
+      )}
+      <div className="heading">
+        {selectedBook && (
           <div>
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-                placeholder="Enter search term"
-              />
-              <button onClick={handleSearch}>Search</button>
-            </form>
-            <div className="add-book">
-              <button type="submit">Add Book</button>
+            <div>
+              <h2 className="title">Supplier's Cart</h2>
+              <p>
+                Title: {selectedBook?.volumeInfo?.title || selectedBook?.title}
+              </p>
+              <p>
+                Author:{" "}
+                {selectedBook?.volumeInfo?.authors[0] || selectedBook?.author}
+              </p>
+              <p>Quantity: {orderQuantity}</p>
+              <p>
+                Price:{" "}
+                {selectedBook?.saleInfo?.listPrice?.amount *
+                  Number(orderQuantity) ||
+                  selectedBook?.cost * Number(orderQuantity) ||
+                  8.99 * Number(orderQuantity)}
+              </p>
+              <button type="submit" onClick={() => handleSubmit(selectedBook)}>
+                Place Order to Supplier
+              </button>
             </div>
-            {books?.map((book) => (
-              <li key={book.id}>
-                <div onClick={() => handleBookClick(book)}>
-                  <a
-                    href={book.volumeInfo.infoLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img
-                      src={book.volumeInfo.imageLinks?.thumbnail}
-                      alt="Book Thumbnail"
-                    />
-                  </a>
-                  <div>
-                    <h3>
-                      <a
-                        href={book.volumeInfo.infoLink}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {book.volumeInfo.title}
-                      </a>
-                    </h3>
-                    <p>{book.volumeInfo.description}</p>
-                    <p>Price: {book.saleInfo?.listPrice?.amount}</p>
-                  </div>
-                </div>
-                <form onSubmit={handleOrderQuantityChange}>
-                  <input
-                    type="number"
-                    value={orderQuantity}
-                    onChange={handleOrderQuantityChange}
-                    placeholder="Quantity"
-                  />
-                  <button type="submit">Order</button>
-                </form>
+          </div>
+        )}
+        <div>
+          <h2 className="title">Search Books</h2>
+
+          <ul>
+            {selectedCollections.map((collectionName, i) => (
+              <li key={`collectionName-${i}`} className="list">
+                <input
+                  type="radio"
+                  name="collection"
+                  value={collectionName}
+                  onClick={() => fetchCollection(collectionName, setBooks)}
+                />
+                {collectionName}
               </li>
             ))}
+          </ul>
+
+          <div>
+            <input
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Enter search term"
+            />
+            <button onClick={handleSearch}>Search</button>
+
+            <div className="add-book"></div>
           </div>
         </div>
       </div>
-      {selectedBook && (
-        <div>
-          <h2>Selected Book</h2>
-          <p>Title: {selectedBook.volumeInfo.title}</p>
-          <p>Author: {selectedBook.volumeInfo.authors[0]}</p>
-          <p>Quantity: {quantity}</p>
-          <p>Price: {selectedBook.saleInfo?.listPrice?.amount}</p>
-          <input
-            type="number"
-            value={orderQuantity}
-            onChange={handleOrderQuantityChange}
-            placeholder="Quantity"
-          />
 
-          <button type="submit">Order Book</button>
-        </div>
-      )}
+      {books &&
+        books?.map((book) => (
+          <li key={book?.id} className="list">
+            <div className="list">
+              <img
+                src={book?.volumeInfo?.imageLinks?.thumbnail || book?.thumbnail}
+                alt="Book Thumbnail"
+                height="200px"
+              />
 
-      {orderSuccess && <p>Success!</p>}
-      {orderError && <p>Error placing order. Please try again.</p>}
+              <div>
+                <h3>{book?.volumeInfo?.title || book?.title}</h3>
+                <p>{book?.volumeInfo?.description || book?.description}</p>
+                <p>
+                  Price:{" "}
+                  {book?.saleInfo?.listPrice?.amount || book?.cost || "8.99"}
+                </p>
+              </div>
+            </div>
+
+            <input
+              type="number"
+              value={orderQuantity}
+              onChange={(event) => setOrderQuantity(event.target.value)}
+              placeholder="Quantity"
+            />
+            <button type="submit" onClick={() => setSelectedBook(book)}>
+              Place in Cart
+            </button>
+          </li>
+        ))}
     </div>
   );
 };
--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,78 +1,85 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import db from "../../provider/Dexie"; 
+import db from "../../provider/Dexie";
 
 const Inventory = () => {
-const navigate = useNavigate();
-const [selectedCategory, setSelectedCategory] = useState("");
-const [searchResults, setSearchResults] = useState([]);
+  const navigate = useNavigate();
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-const selectedCollections = [
-"bestSeller",
-"newReleases",
-"carousel",
-"employeeRecommendations",
-];
+  const selectedCollections = [
+    "bestSeller",
+    "newReleases",
+    "carousel",
+    "employeeRecommendations",
+  ];
 
-const fetchBooks = async () => {
-if (selectedCategory === "") {
-setSearchResults([]);
-return;
-}
+  const fetchBooks = async () => {
+    if (selectedCategory === "") {
+      setSearchResults([]);
+      return;
+    }
 
-const results = await db[selectedCategory]?.toArray();
-setSearchResults(results);
+    const results = await db[selectedCategory]?.toArray();
+    setSearchResults(results);
+  };
 
-};
+  useEffect(() => {
+    fetchBooks();
+  }, [selectedCategory]);
 
-useEffect(() => {
-fetchBooks();
-}, [selectedCategory]);
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+  };
 
-const handleCategorySelect = (category) => {
-setSelectedCategory(category);
-};
-
-return (
-<div>
-  <div className="inventory">
-    <div className="inventory-container"> 
-      <h1>Store Inventory</h1>
-      <div className="collection-buttons">
-        {selectedCollections?.map((collectionName) => (
-          <button
-            key={collectionName}
-            onClick={() => handleCategorySelect(collectionName)}
-            className={selectedCategory === collectionName ? "active" : ""}
-          >
-            {collectionName}
-          </button>
-        ))}
-      </div>
-  
-      <div className="inventory-list">
-        {searchResults?.map((result) => (
-          <div key={result?.id} className="inventory-item">
-            <img src={result?.thumbnail} alt={result?.title} height="200px" />
-            <div className="item-details">
-              <h2>{result?.title}</h2>
-              <p
-                style={{
-                  color: result?.inventoryCount <= 2 ? "red" : "inherit",
-                }}
+  return (
+    <div>
+      <div className="inventory">
+        <div className="inventory-container">
+          <h1>Store Inventory</h1>
+          <div className="collection-buttons">
+            {selectedCollections?.map((collectionName) => (
+              <button
+                key={collectionName}
+                onClick={() => handleCategorySelect(collectionName)}
+                className={selectedCategory === collectionName ? "active" : ""}
               >
-                Inventory Count: {result?.inventoryCount}
-              </p>
-            </div>
+                {collectionName}
+              </button>
+            ))}
           </div>
-        ))}
-      </div>
 
-      <button onClick={() => navigate("/")}>Go back</button>
+          <div className="inventory-list">
+            {searchResults?.map((result) => (
+              <div key={result?.id} className="inventory-item">
+                {result?.thumbnail ? (
+                  <img
+                    src={result?.thumbnail}
+                    alt={result?.title}
+                    height="200px"
+                  />
+                ) : (
+                  <div className="no-image">{result?.title}</div>
+                )}
+                <div className="item-details">
+                  <h2>{result?.title}</h2>
+                  <p
+                    style={{
+                      color: result?.inventoryCount <= 2 ? "red" : "inherit",
+                    }}
+                  >
+                    Inventory Count: {result?.inventoryCount}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <button onClick={() => navigate("/")}>Go back</button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-);
+  );
 };
 
 export default Inventory;
